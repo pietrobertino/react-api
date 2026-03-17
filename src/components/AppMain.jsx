6b0938a92795127a055334889196b2b2561f26ea
pietrobertino <pietrobertino20@gmail.com>
2026-03-17 12:47:25 +0100

    Complete second milestone

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export default function AppMain({ url_api }) {
+
+    const [actors, setActors] = useState([]);
 
     useEffect(() => {
         fetch(url_api)
             .then(response => response.json())
             .then(data => {
                 console.log(data)
+                setActors(data)
             })
     }, [])
 
@@ -14,11 +17,23 @@ export default function AppMain({ url_api }) {
         <main>
             <div className="container">
                 <div className="text-center">
-                    <h1>Actresses</h1>
-                    <p>List of actresses fetched from an API</p>
+                    <h1>Actors</h1>
+                    <p>List of actors fetched from an API</p>
                 </div>
 
-
+                <div>
+                    {actors.map(actor => (
+                        <div key={actor?.id} className=" card p-3">
+                            <div className=" card-title">{actor.name}</div>
+                            <img src={actor.image} alt={actor.name} className=" card-img" />
+                            <div className=" card-subtitle">{actor.birth_year} {actor.nationality}</div>
+                            <div className=" card-body">
+                                <p className="card-text">{actor.biography}</p>
+                                <div className=" card-text">Awards: <br /> {actor.awards} </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </main>
     )
